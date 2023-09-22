b218f0ed2a5d1308e22930c9394a46ce3308cc45
shiva5152 <sg2304366@gmail.com>
2023-09-22 16:14:53 +0530

    dashboard saved

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,25 @@
 "use client";
 import { useAuth } from "@/context/AuthContext";
+import { useEffect } from "react";
 // import { useRouter } from "next/navigation";
-import { usePathname } from "next/navigation";
-
+import { usePathname, useRouter } from "next/navigation";
+import Loader from "@/components/Loder";
 export default function Home() {
   const Pathname = usePathname();
   if (Pathname === "/") {
     console.log("path name is this ");
   }
+  const router = useRouter();
+  useEffect(() => {
+    router.push("/dashboard");
+  }, []);
 
   const { logout } = useAuth();
   return (
     <div className="bg-[#fbfbfb] h-screen">
-      <button onClick={logout}></button>
+      <div className="w-full flex justify-center items-center h-screen mx-auto">
+        <Loader />
+      </div>
     </div>
   );
 }
